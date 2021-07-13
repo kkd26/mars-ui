bdccc68695f9f85dd936cc1313e8fe26ef60ec0d
Krzysztof Druciarek <krzysztof.druciarek@softwire.com>
2021-07-13 14:48:38 +0100

    Button counter with react hooks and local storage

---
+++ src/ButtonCounter.tsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+import "./ButtonCounter.scss";
+
+export default function ButtonCounter() {
+    
+  const initCount = Number(window.localStorage.getItem("count"));
+
+  const [count, setCount] = useState(initCount);
+
+  useEffect(() => {
+    window.localStorage.setItem("count", count.toString());
+  }, [count]);
+
+  function click() {
+    setCount((c) => c + 1);
+  }
+
+  return (
+    <div className="counter">
+      <div>Current count: {count}</div>
+      <button onClick={click}>Click me!</button>
+    </div>
+  );
+}
