c51d2059f2df61b13c4b724b349c5f300c1b0789
Krzysztof Druciarek <krzysztof.druciarek@softwire.com>
2021-07-16 11:14:13 +0100

    Fix Camera select and fetch api

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import API from "../Api";
 import "../styles/App.scss";
 import CameraSelect from "./CameraSelect";
 import Gallery from "./Gallery";
 import RoverSelect from "./RoverSelect";
+import { PhotoProp } from "./Photo";
 
 const App = () => {
   const [selectedRover, setSelectedRover] = useState("");
@@ -12,31 +13,49 @@ const App = () => {
     setSelectedRover(rover.value);
   };
 
-  const [selectedCamera, setSelectedCamera] = useState<string[]>([]);
+  const [cameraPromise, setCameraPromise] = useState<string[]>([]);
+
+  useEffect(() => {
+    if (selectedRover) {
+      API.getRoverCameras(selectedRover).then((c) => setCameraPromise(c));
+      console.log("new promise");
+      setSelectedCamera([]);
+    }
+  }, [selectedRover]);
+
+  const [selectedCamera, setSelectedCamera] = useState<
+    { value: string; label: string }[]
+  >([]);
 
   const handleCameraSelect = (camera: { label: string; value: string }[]) => {
-    const cameras = camera.map((c) => c.value);
-    setSelectedCamera(cameras);
-    getPhotos();
+    setSelectedCamera(camera);
   };
 
-  const [photos, setPhotos] = useState([]);
+  useEffect(() => {
+    getPhotos();
+  }, [selectedCamera]);
+
+  const [photos, setPhotos] = useState<PhotoProp[]>([]);
 
   const getPhotos = () => {
-    API.getRoverPhotos(selectedRover, selectedCamera[0]).then((photos) => {
-      setPhotos(photos);
+    console.log("fetch photos");
+    setPhotos([]);
+    selectedCamera.forEach((camera) => {
+      API.getRoverPhotos(selectedRover, camera.value).then((newPhotos) => {
+        setPhotos([...photos, ...newPhotos]);
+      });
     });
   };
-
-  // useEffect(() => {
-  //   return;
-  // }, [selectedRover]);
 
   return (
     <>
       <RoverSelect callOnChange={handleRoverSelect} />
       {selectedRover && (
-        <CameraSelect callOnChange={handleCameraSelect} rover={selectedRover} />
+        <CameraSelect
+          callOnChange={handleCameraSelect}
+          cameraPromise={cameraPromise}
+          selectedCamera={selectedCamera}
+        />
       )}
       <Gallery photos={photos} />
     </>
--- src/components/CameraSelect.tsx
+++ src/components/CameraSelect.tsx
@@ -1,33 +1,26 @@
 import React from "react";
-import Select from "react-select/async";
-import API from "../Api";
+import Select from "react-select";
 
 const CameraSelect = ({
-  rover,
   callOnChange,
+  cameraPromise,
+  selectedCamera,
 }: {
-  rover: string;
   callOnChange?: any;
+  cameraPromise: string[];
+  selectedCamera: { value: string; label: string }[];
 }) => {
-  const p = API.getRoverCameras(rover);
-
-  const cameraFetch = (cameraName: string) =>
-    p.then((res) =>
-      res
-        .filter((camera) =>
-          camera.toLocaleLowerCase().includes(cameraName.toLocaleLowerCase())
-        )
-        .map((camera) => {
-          return { value: camera, label: camera };
-        })
-    );
+  const cameraFetch = cameraPromise.map((camera) => {
+    return { value: camera, label: camera };
+  });
 
   return (
     <Select
       isMulti
       cacheOptions
       defaultOptions
-      loadOptions={cameraFetch}
+      value={selectedCamera}
+      options={cameraFetch}
       onChange={callOnChange}
     />
   );
